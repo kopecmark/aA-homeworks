457f4f5ba39252fe9397ad90d0ae2ba93590508d
Mark Kopec <mark.kopec@gmail.com>
2018-10-25 22:22:09 -0700

    Add logic to all queue class methods

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -23,20 +23,21 @@ end
 
 
 class Queue
+  attr_reader :queue
 
   def initialize
     @queue = []
   end
 
   def enqueue(el)
-
+    queue.push(el)
   end
 
   def dequeue
-
+    queue.shift
   end
 
   def peek
-
+    queue.first
   end
 end
