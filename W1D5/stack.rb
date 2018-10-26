3d4966b7aeb1fcfcf184f94968ac2ba60176d2e9
Mark Kopec <mark.kopec@gmail.com>
2018-10-25 22:18:31 -0700

    Add queue class

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -20,3 +20,23 @@ class Stack
     stack.last
   end
 end
+
+
+class Queue
+
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+
+  end
+
+  def dequeue
+
+  end
+
+  def peek
+
+  end
+end
