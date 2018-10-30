1f4d5b91b9a31d6e4ffb191c04c2f5841c76e002
Mark Kopec <mark.kopec@gmail.com>
2018-10-29 22:49:38 -0700

    Add board methods

--- W2D2/mancala/lib/board.rb
+++ W2D2/mancala/lib/board.rb
@@ -1,21 +1,58 @@
+require 'byebug'
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1, @name2 = name1, name2
+    @cups = Array.new(14) {Array.new()}
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    cups.map.with_index do |cup, idx|
+      next if idx == 6 || idx == 13
+      4.times do
+        cup << :stone
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos > 13
+    raise "Starting cup is empty" if cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
+    stones = cups[start_pos]
+    cups[start_pos] = []
+    idx = start_pos
+    until stones.empty?
+      idx += 1
+      idx = 0 if idx > 13
+      if idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[idx] << stones.pop
+      end
+    end
+    render
+    next_turn(idx)
   end
+
+
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elseif cups[ending_cup_idx].length == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
